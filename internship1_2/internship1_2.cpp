01af5fe302bed0bac824a88ffebac085e980e715
tori-3 <syokatukoumei1223@docomo.ne.jp>
2024-08-02 17:44:45 +0900

    プロジェクト ファイルを追加します。

---
+++ internship1_2/internship1_2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream> 
+#include"List.h"
+
+int main()
+{
+    //テストコード
+    List list;
+
+    std::ifstream file;
+    std::string line;
+
+    file.open("Scores.txt", std::ios::in);
+
+    //一行ずつ読み込む
+    while (std::getline(file, line))
+    {
+        Grades grades;
+
+        //タブで区切り、リストに追加する。
+        std::string temp;
+        std::stringstream ss{ line };
+        std::getline(ss, temp, '\t');
+        grades.score = atoi(temp.c_str());
+        std::getline(ss, temp, '\t');
+        grades.name = temp;
+
+        list.add(grades);
+    }
+
+    for (auto it = list.constBegin(); it != list.constEnd(); ++it)
+    {
+        std::cout << (*it).score << "\t" << (*it).name << std::endl;
+    }
+}
